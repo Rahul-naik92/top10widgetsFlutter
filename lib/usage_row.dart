f52460a3215a8065f16dcfc3da54146fcd86903f
Rahul-naik92 <team.rahulnaik@gmail.com>
2023-03-26 16:19:46 +0530

    usage of Circular Widget

--- lib/usage_row.dart
+++ lib/usage_row.dart
@@ -8,42 +8,20 @@ class MyAppDemo extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Usage of Stack Widget"),
+          title: Text("Usage of Circle Avatra Widget"),
         ),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Stack(
-              children: [
-                SizedBox(
-                  child: Stack(
-                    children: [
-                      Text("Rahul Naik"),
-                      Container(
-                        height: 300,
-                        width: 100,
-                        color: Colors.green,
-                      ),
-                    ],
-                  ),
-                ),
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.red,
-                ),
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.red,
-                ),
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.red,
-                ),
-              ],
+            Center(
+              child: CircleAvatar(
+                maxRadius: 100,
+                minRadius: 40,
+                backgroundColor: Colors.orange,
+                backgroundImage: NetworkImage(
+                    "https://images.pexels.com/photos/6147118/pexels-photo-6147118.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"),
+              ),
             )
           ],
         ),
@@ -51,3 +29,34 @@ class MyAppDemo extends StatelessWidget {
     );
   }
 }
+/*Stack(
+children: [
+SizedBox(
+child: Stack(
+children: [
+Text("Rahul Naik"),
+Container(
+height: 300,
+width: 100,
+color: Colors.green,
+),
+],
+),
+),
+Container(
+height: 100,
+width: 100,
+color: Colors.red,
+),
+Container(
+height: 100,
+width: 100,
+color: Colors.red,
+),
+Container(
+height: 100,
+width: 100,
+color: Colors.red,
+),
+],
+)*/
